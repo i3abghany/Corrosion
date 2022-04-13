849387dab43820a61b941ca00ca66b2100226a83
Mahmoud Mandour <Mahmoudabdalghany@outlook.com>
2022-04-13 20:30:28 +0200

    Define common registers in the source file.

--- Corrosion/Operand.cpp
+++ Corrosion/Operand.cpp
@@ -1,1 +1,21 @@
 #include "Operand.h"
+
+const Operand rax = {
+   Operand::OperandType::REGISTER_OPERAND,
+   rax_reg
+};
+
+const Operand rcx = {
+	Operand::OperandType::REGISTER_OPERAND,
+	rcx_reg
+};
+
+const Operand rbx = {
+	Operand::OperandType::REGISTER_OPERAND,
+	rbx_reg
+};
+
+const Operand rdx = {
+	Operand::OperandType::REGISTER_OPERAND,
+	rdx_reg
+};
--- Corrosion/Operand.h
+++ Corrosion/Operand.h
@@ -23,22 +23,7 @@ struct Operand
 	};
 };
 
-const Operand rax = {
-	Operand::OperandType::REGISTER_OPERAND,
-	rax_reg
-};
-
-const Operand rcx = {
-	Operand::OperandType::REGISTER_OPERAND,
-	rcx_reg
-};
-
-const Operand rbx = {
-	Operand::OperandType::REGISTER_OPERAND,
-	rbx_reg
-};
-
-const Operand rdx = {
-	Operand::OperandType::REGISTER_OPERAND,
-	rdx_reg
-};
+extern const Operand rax;
+extern const Operand rcx;
+extern const Operand rbx;
+extern const Operand rdx;
